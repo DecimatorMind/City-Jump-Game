517c102b0874bbec3bb33b4247f7aee85a316aee
Pranjal Bhardwaj <decimatormind@gmail.com>
2021-09-23 09:25:09 +0530

    Update Scripts

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private Vector3 startpos;
+    private float speed = 10;
+    private float repeatwidth;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startpos = transform.position;
+        repeatwidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if(transform.position.x < startpos.x - repeatwidth)
+        {
+            transform.position = startpos;
+        }
     }
 }
