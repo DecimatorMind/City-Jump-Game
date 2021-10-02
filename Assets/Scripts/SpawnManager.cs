5859fa8fe8121b9cfc88ab8b130c401cd30ba5af
Pranjal Bhardwaj <decimatormind@gmail.com>
2021-10-02 19:44:44 +0530

    Add Text Mesh Score

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpawnManager : MonoBehaviour
 {
-
-    public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(30, 0, 0);
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController playerControllerScript;
+    public GameObject[] prefabArray;
+    public float score;
+    public TMP_Text textMeshPro;
     // Start is called before the first frame update
     void Start()
     {
+        textMeshPro.text = "Score: 0";
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle",startDelay,repeatRate);
     }
@@ -20,14 +23,16 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        textMeshPro.text = "Score: " + score;
     }
 
     void SpawnObstacle()
     {
         if(playerControllerScript.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            int index = Random.Range(0, prefabArray.Length);
+            Instantiate(prefabArray[index], spawnPos, prefabArray[index].transform.rotation);
+            score += (index + 1);
         }
     }
 }
